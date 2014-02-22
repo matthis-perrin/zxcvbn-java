04261b439916f9a760d64ef787dcbdc8ee347786
Matthis <matthis.perrin@gmail.com>
2014-02-21 17:27:58 -0800

    Add a RepeatMatch and a SequenceMatch

---
+++ src/zxcvbn/java/matching/RepeatMatch.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 Matthis Perrin <matthis.perrin at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zxcvbn.java.matching;
+
+/**
+ *
+ * @author Matthis Perrin <matthis.perrin at gmail.com>
+ */
+public class RepeatMatch extends BasicMatch {
+  
+  
+  private final char character;
+  private final int repeat;
+  
+  
+  /**
+   * Create a new <code>RepeatMatch</code> which is a <code>String</code>
+   * repeating the same <code>char</code>
+   * @param match a <code>String</code> repeating a <code>char</code>
+   */
+  public RepeatMatch (String match) {
+    super(match);
+    this.character = match.charAt(0);
+    this.repeat = match.length();
+  }
+
+  
+  
+  /**
+   * @return the character that is repeated in the token
+   */
+  public char getCharacter () {
+    return character;
+  }
+
+  
+  /**
+   * @return how many time the character is repeating in the token
+   */
+  public int getRepeat () {
+    return repeat;
+  }
+  
+  
+}
---
+++ src/zxcvbn/java/matching/SequenceMatch.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2014 Matthis Perrin <matthis.perrin at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zxcvbn.java.matching;
+
+/**
+ *
+ * @author Matthis Perrin <matthis.perrin at gmail.com>
+ */
+public class SequenceMatch extends BasicMatch {
+  
+  
+  private final char firstCharacter;
+  private final int length;
+  private final boolean asc;
+  
+  
+  /**
+   * Create a new <code>SequenceMatch</code> which is a sequence of characters 
+   * that are following each other (e.g. abcd, 987, RST...)
+   * @param match a <code>String</code> that is a sequence of characters
+   * @param asc determine if the sequence is ascending (true) or descending 
+   *            (false)
+   */
+  public SequenceMatch (String match, boolean asc) {
+    super(match);
+    this.firstCharacter = match.charAt(0);
+    this.length = match.length();
+    this.asc = asc;
+  }
+
+  
+  
+  /**
+   * @return the first character of the sequence
+   */
+  public char getFirstCharacter () {
+    return firstCharacter;
+  }
+
+  
+  /**
+   * @return the length of the sequence
+   */
+  public int getLength () {
+    return length;
+  }
+  
+  
+  /**
+   * @return <code>true</code> if the sequence is ascending, <code>flase</code>
+   *         otherwise
+   */
+  public boolean isAscending () {
+    return asc;
+  }
+  
+  
+}
