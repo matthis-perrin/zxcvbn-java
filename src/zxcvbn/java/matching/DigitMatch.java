876eb1d2daf03bf2fa23786257b537851df3ff32
Matthis <matthis.perrin@gmail.com>
2014-02-21 17:28:34 -0800

    Add DigitMatch and SequenceMatch

---
+++ src/zxcvbn/java/matching/DigitMatch.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 Matthis Perrin <matthis.perrin at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zxcvbn.java.matching;
+
+/**
+ *
+ * @author Matthis Perrin <matthis.perrin at gmail.com>
+ */
+public class DigitMatch extends BasicMatch {
+  
+  
+  private final int length;
+  
+  
+  /**
+   * Create a new <code>DigitMatch</code> which is a <code>String</code>
+   * containing only digits
+   * @param match a <code>String</code> containing only digits
+   */
+  public DigitMatch (String match) {
+    super(match);
+    this.length = match.length();
+  }
+
+  
+  /**
+   * @return the length of the match
+   */
+  public int getLength () {
+    return length;
+  }
+  
+  
+}
---
+++ src/zxcvbn/java/matching/YearMatch.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014 Matthis Perrin <matthis.perrin at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zxcvbn.java.matching;
+
+/**
+ *
+ * @author Matthis Perrin <matthis.perrin at gmail.com>
+ */
+public class YearMatch extends BasicMatch {
+
+  
+  /**
+   * Create a new <code>YearMatch</code> which is a <code>String</code>
+   * that represents a year.
+   * @param match a <code>String</code> representing a year
+   */
+  public YearMatch (String match) {
+    super(match);
+  }
+  
+  
+}
